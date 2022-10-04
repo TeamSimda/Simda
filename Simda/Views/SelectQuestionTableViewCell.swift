3a9beb8b173e5420e906aeccba24c3ad013809fc
sm-amoled <moktak072@naver.com>
2022-10-05 03:15:54 +0900

    🚧#8 - MakingStoryView 레이아웃 WIP
    텍스트 입력 상태에 따라 뷰 레이아웃을 유동적으로 변형하는 기능 구현중

---
+++ Simda/Views/SelectQuestionTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SelectQuestionTableViewCell.swift
+//  Simda
+//
+//  Created by Park Sungmin on 2022/10/05.
+//
+
+import Foundation
+import UIKit
+
+class SelectQuestionTableViewCell: UITableViewCell {
+    @IBOutlet weak var questionLabel: UILabel!
+}
---
+++ Simda/Views/WriteQuestionTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  WriteQuestionTableViewCell.swift
+//  Simda
+//
+//  Created by Park Sungmin on 2022/10/05.
+//
+
+import Foundation
