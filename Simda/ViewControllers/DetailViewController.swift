bd399938625a03e055dbbf10826354ee6ed3eb7f
Jeon Jimin <41153398+JMM00@users.noreply.github.com>
2022-10-13 21:28:01 +0900

    🚧#5 - DetailView UI WIP

---
+++ Simda/ViewControllers/DetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DetailViewController.swift
+//  Simda
+//
+//  Created by 전지민 on 2022/10/13.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var seedName: UILabel!
+    @IBOutlet weak var keyword1: TagLabel!
+    @IBOutlet weak var keyword2: TagLabel!
+    @IBOutlet weak var keyword3: TagLabel!
+    
+    @IBOutlet weak var question1: QuestionLabel!
+    @IBOutlet weak var question2: QuestionLabel!
+    @IBOutlet weak var question3: QuestionLabel!
+    
+    @IBOutlet weak var diaryContent: UILabel!
+    @IBOutlet weak var diaryTitle: UILabel!
+    
+    @IBOutlet weak var contentBackgroundView: UIView!
+    @IBOutlet weak var evolveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setUI()
+        setSeedDetail()
+    }
+    private func setUI() {
+        keyword1.clipsToBounds = true
+        keyword1.layer.cornerRadius = 14
+        keyword2.clipsToBounds = true
+        keyword2.layer.cornerRadius = 14
+        keyword3.clipsToBounds = true
+        keyword3.layer.cornerRadius = 14
+        
+        question1.clipsToBounds = true
+        question1.layer.cornerRadius = 10
+        question2.clipsToBounds = true
+        question2.layer.cornerRadius = 10
+        question3.clipsToBounds = true
+        question3.layer.cornerRadius = 10
+        
+        contentBackgroundView.layer.cornerRadius = 10
+        evolveButton.clipsToBounds = true
+        evolveButton.layer.cornerRadius = 25
+    }
+    private func setSeedDetail() {
+        self.seedName.text = "test"
+        self.seedName.textColor = UIColor(named: "Red")
+        self.keyword1.text = "#key1"
+        self.keyword2.text = "#key2"
+        self.keyword3.text = "#key3"
+        self.question1.text = "question1"
+        self.question2.text = "question2"
+        self.question3.text = "question3"
+        self.diaryTitle.text = "냠냠한 하루"
+        self.diaryContent.text = "아아아아ㅏ아아나는 일기를 쓴다"
+        self.evolveButton.tintColor = UIColor(named: "Red")
+    }
+
+
+}
+
